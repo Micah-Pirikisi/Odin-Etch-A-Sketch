946798389d0df87a54bab34c1e89e4e5199c5399
Micah-Pirkisi <micahpirikisi@gmail.com>
2025-09-09 09:48:30 +0200

    added button and prompt allowing users to customize grid size.

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,9 +1,30 @@
 const main = document.querySelector('main'); 
+const body = document.querySelector('body'); 
 
-for (let i = 0; i < 16 * 16; i++) {
+const button = document.createElement('button'); 
+button.textContent = 'create new grid'; 
+
+function clickHandler(e) {
+    let choice;
+    do {
+        choice = parseInt(prompt('Number of squares per side for your grid (1–100):'));
+    } while (isNaN(choice) || choice <= 0 || choice > 100);
+    
+    main.innerHTML = ''; 
+
+    const squareSize = 960 / choice + 'px'; 
+
+    for (let i = 0; i < choice * choice; i++) {
     const square = document.createElement('div'); 
     square.classList.add('grid-square'); 
+    square.style.width = squareSize;
+    square.style.height = squareSize;
     main.appendChild(square); 
-
     square.addEventListener('mouseover', () => square.style.backgroundColor = 'rgba(192, 192, 192, 0.9)'); 
-}
+    }
+}; 
+
+button.addEventListener('click', clickHandler); 
+body.insertBefore(button, main); 
+
+document.addEventListener('DOMContentLoaded', clickHandler)
